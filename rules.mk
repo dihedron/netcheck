d89621259b8f05acb6ae72ddb41a8b852e7b9ca7
Andrea Funtò <dihedron.dev@gmail.com>
2024-12-08 00:16:04 +0100

    Updated rules.mk

--- rules.mk
+++ rules.mk
@@ -2,14 +2,14 @@
 # This value is updated each time a new feature is added
 # to the rules.mk targets and build rules file.
 #
-_RULES_MK_CURRENT_VERSION := 202412061025
+_RULES_MK_CURRENT_VERSION := 202412080015
 ifeq ($(_RULES_MK_MINIMUM_VERSION),)
 	_RULES_MK_MINIMUM_VERSION := 0
 endif
 
-# 
+#
 # test if minimum rules.mk version requirement is met
-# 
+#
 ifneq ($(shell test $(_RULES_MK_CURRENT_VERSION) -ge $(_RULES_MK_MINIMUM_VERSION); echo $$?),0)
 	@echo "minimum rules.mk version requirement not met (expected at least $(_RULES_MK_MINIMUM_VERSION), got $(_RULES_MK_CURRENT_VERSION))" && exit 1
 endif
@@ -123,7 +123,7 @@ endif
 #
 # Set this flag to 1 if you want to force the rebuild of all dependencies
 # even if they are up-to-date. This can be useful when changing the value
-# of CGO, in order to make sure that all object files (.a) are compiled 
+# of CGO, in order to make sure that all object files (.a) are compiled
 # with the desired settings.
 #
 ifneq ($(_RULES_MK_FORCE_DEP_REBUILD),1)
@@ -223,13 +223,13 @@ endif
 ifeq ($(_RULES_MK_STRIP_SYMBOLS),1)
 	@echo -e " - strip symbols    : $(yellow)yes$(reset)"
 	$(eval strip_symbols=-s)
-else 
+else
 	@echo -e " - strip symbols    : $(green)no$(reset)"
 endif
 ifeq ($(_RULES_MK_STRIP_DBG_INFO),1)
 	@echo -e " - strip debug info : $(yellow)yes$(reset)"
 	$(eval strip_dbg_info=-w)
-else 
+else
 	@echo -e " - strip debug info : $(green)no$(reset)"
 endif
 ifeq ($(_RULES_MK_STRIP_SYMBOLS),1)
@@ -513,6 +513,15 @@ supported: ## show supported build platforms
 		fi; \
 	done
 	@rm -f .piped
+
+.PHONY: setup-tools
+setup-tools: ## install all necessary tools at the latest version
+	@go install golang.org/x/vuln/cmd/govulncheck@latest
+	@go install github.com/securego/gosec/v2/cmd/gosec@latest
+	@go install golang.org/x/tools/go/analysis/passes/shadow/cmd/shadow@latest
+	@go install honnef.co/go/tools/cmd/staticcheck@latest
+	@go install github.com/mattn/goreman@latest
+	@go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.62.2
 
 # this in an internal task used to detect whether the main Make
 # instance is running in an interactive shell or redirected/piped
